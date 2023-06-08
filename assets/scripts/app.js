b03d0e52f98fa198171e3c773beafc7c24f4bd51
Oleksii <stinovoleksij@gmail.com>
2023-06-08 14:35:06 +0300

    Added a modal validation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,45 @@
-const addMovieModal = document.getElementById("add-modal");
+const addMovieModal = document.getElementById('add-modal');
 // const startAddMovieButton = document.querySelector("header").lastElementChild;
-const startAddMovieButton = document.querySelector("header button");
+const startAddMovieButton = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
+const cancelAddMovieBtn = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieBtn.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll('input');
 
-const toggleMovieModal = () => {
-  addMovieModal.classList.toggle("visible");
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible');
 };
 
-startAddMovieButton.addEventListener("click", toggleMovieModal);
+const cancelAddMovieHandler = () => {
+  toggleMovieModal();
+};
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle('visible');
+  toggleBackdrop();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+  if (
+    titleValue.trim() === '' ||
+    imageUrlValue === '' ||
+    ratingValue === '' ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert('Please enter the value between 1 and 5');
+    return;
+  }
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieBtn.addEventListener('click', cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener('click', addMovieHandler);
