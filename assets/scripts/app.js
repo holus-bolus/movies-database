6cacebcf5a42acf5c737b9ec2b81a6e267009693
Oleksii <stinovoleksij@gmail.com>
2023-06-08 16:45:25 +0300

    Added movies on the page

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,6 +5,39 @@ const backdrop = document.getElementById('backdrop');
 const cancelAddMovieBtn = addMovieModal.querySelector('.btn--passive');
 const confirmAddMovieButton = cancelAddMovieBtn.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
+const entryTextSection = document.getElementById('entry-text');
+
+const movies = [];
+
+const clearMovieInputs = () => {
+  for (const userInput of userInputs) {
+    userInput.value = '';
+  }
+};
+
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = 'block';
+  } else {
+    entryTextSection.style.display = 'none';
+  }
+};
+
+const renderNewMovieElement = (title, imageURL, rating) => {
+  const newMovieElement = document.createElement('li');
+  newMovieElement.className = 'movie-element';
+  newMovieElement.innerHTML = `
+     <div class="movie-element__image">
+            <img src="${imageURL}" alt="${title}">
+      </div>
+      <div class="movie-element__info">
+            <h2>${title}</h2>
+            <p>${rating} / 5</p>
+        </div>
+  `;
+  const listRoot = document.getElementById('movie-list');
+  listRoot.appendChild(newMovieElement);
+};
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle('visible');
@@ -12,6 +45,7 @@ const toggleBackdrop = () => {
 
 const cancelAddMovieHandler = () => {
   toggleMovieModal();
+  clearMovieInputs();
 };
 
 const toggleMovieModal = () => {
@@ -37,6 +71,17 @@ const addMovieHandler = () => {
     alert('Please enter the value between 1 and 5');
     return;
   }
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModal();
+  clearMovieInputs();
+  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+  updateUI();
 };
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
